169237f3ef6997c45df75b7317deee4e8fbd040f
Tal Koren <talko4245@gmail.com>
2021-10-27 16:42:41 +0300

    finish q2

---
+++ task2/task2_q1b/main.c
@@ -0,0 +1,149 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 100
+
+void pointerSort(int *arr, unsigned int size, int ascend_flag, int ***pointers);
+void mergeSortP(int **arr, int size, int flag);                      // sort array using merage sort algorithm working with local array
+int **merge(int **arr1, int size1, int **arr2, int size2, int flag); // used in mergeSortP - combine the part of the array in sorted way according to flag value
+void copyArr(int **dest, int **src, int size);                       // used in mergeSortP - copies the local array of mergeSortP function to the return array
+int compare(int a, int b, int flag);                                 // used in merge function - return true/false according to the flag value
+void printPointers(int **pointersArr, int size);
+
+void main()
+{
+
+    unsigned int size, i;
+    int arr[SIZE];
+    int **pointers;
+    int ascend_flag;
+
+    printf("Please enter the number of items:\n");
+    scanf("%u", &size);
+
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &ascend_flag);
+
+    pointerSort(arr, size, ascend_flag, &pointers);
+
+    printf("The sorted array:\n"); // Print the sorted array
+
+    printPointers(pointers, size);
+
+    free(pointers);
+}
+
+void pointerSort(int *arr, unsigned int size, int ascend_flag, int ***pointers)
+{
+    int **pointersArr;
+
+    // allocate new memory for the arry of pointers
+    pointersArr = (int **)malloc(sizeof(int *) * size);
+    if (pointersArr == NULL)
+    {
+        printf("Error - memory allocation faild\n");
+        exit(1);
+    }
+
+    // init the array of pointers
+    for (int i = 0; i < size; i++)
+    {
+        pointersArr[i] = arr + i;
+    }
+
+    mergeSortP(pointersArr, size, ascend_flag);
+    *pointers = pointersArr;
+}
+
+void mergeSortP(int **arr, int size, int flag)
+{
+    int **tempArr = NULL;
+
+    if (size <= 1)
+    {
+        return;
+    }
+    mergeSortP(arr, size / 2, flag);
+    mergeSortP(arr + size / 2, size - size / 2, flag);
+    tempArr = merge(arr, size / 2, arr + size / 2, size - size / 2, flag);
+    copyArr(arr, tempArr, size); // copy temp arr to the original arr
+    free(tempArr);
+}
+
+int **merge(int **arr1, int size1, int **arr2, int size2, int flag)
+{
+    int index1, index2, indexRes;
+    int **res;
+    index1 = 0;
+    index2 = 0;
+    indexRes = 0;
+
+    // allocate new array to combine the two arrays from the function
+    res = (int **)malloc(sizeof(int *) * (size1 + size2));
+    if (res == NULL)
+    {
+        printf("Error - memory allocation faild!");
+        exit(1);
+    }
+
+    while (index1 < size1 && index2 < size2)
+    {
+        if (compare(*(arr1[index1]), *(arr2[index2]), flag)) // use compare to sort in decending/ ascending order according to the flag
+        {
+            res[indexRes] = arr1[index1];
+            index1++;
+        }
+        else
+        {
+            res[indexRes] = arr2[index2];
+            index2++;
+        }
+        indexRes++;
+    }
+
+    while (index1 < size1)
+    {
+        res[indexRes] = arr1[index1];
+        index1++;
+        indexRes++;
+    }
+
+    while (index2 < size2)
+    {
+        res[indexRes] = arr2[index2];
+        index2++;
+        indexRes++;
+    }
+
+    return res;
+}
+
+void copyArr(int **dest, int **src, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        dest[i] = src[i];
+    }
+}
+
+int compare(int a, int b, int flag)
+{
+    if (flag == 1)
+    {
+        return a < b;
+    }
+
+    return a > b;
+}
+
+void printPointers(int **pointersArr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", *(pointersArr[i]));
+    }
+}
