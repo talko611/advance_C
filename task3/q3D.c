6d6b665f4b2c58ff83075885a58ae4455af54ebe
Tal Koren <talko4245@gmail.com>
2021-11-20 20:57:50 +0200

    finished task 3 - q3d

--- task3/q3D.c
+++ task3/q3D.c
@@ -44,6 +44,14 @@ List getCoordList();
 void freeYList(YList *lst);
 void freeList(List *lst);
 int insertCoordinate(List *coordList, int x, int y);
+void printList(List *lst);
+void insertXNodeAfter(XListNode *after, XListNode *newNode); // insert x node between 2 nodes
+XListNode *findXNode(List *lst, int x, int y, int *found);   // search for same x coordinate if found return match x node if not found create new coordinate and insert in correct place
+void insertDataToStartYList(YList *lst, int data);
+void insertYNodeToStartList(YList *lst, YListNode *newNode);
+void insertDataAfterYnode(YListNode *after, int data);
+void insertYcooradinate(YList *lst, int y, int *found); // checks if y coordinate is exists and add new y to correct location
+int insertCoordinate(List *coordList, int x, int y);
 
 void main()
 {
@@ -56,11 +64,13 @@ void main()
     scanf("%d%d", &x, &y);
     res = insertCoordinate(&coordList, x, y);
 
-    if(res == 0){
-        printf("The point (%d,%d) didn't appear\n",x,y);
+    if (res == 0)
+    {
+        printf("The point (%d,%d) didn't appear\n", x, y);
     }
-    else{
-        printf("The point (%d,%d) already appeared\n",x,y);
+    else
+    {
+        printf("The point (%d,%d) already appeared\n", x, y);
     }
 
     printf("Updated list: ");
@@ -242,6 +252,149 @@ void freeList(List *lst)
     }
 }
 
-int insertCoordinate(List *coordList, int x, int y){
-    
+int insertCoordinate(List *coordList, int x, int y)
+{
+    XListNode *xNode;
+    int res = 0, foundX = 0, foundY = 0;
+
+    xNode = findXNode(coordList, x, y, &foundX); // look for match coordinate or insert new one;
+    if (foundX == 1)
+    {                                                      // case x is found
+        insertYcooradinate(&(xNode->listOfY), y, &foundY); // check if y is found and insert y cooradinate
+        if (foundY == 1)
+        {
+            res = 1;
+        }
+    }
+    return res;
+}
+
+void insertYcooradinate(YList *lst, int y, int *found)
+{
+    YListNode *p1, *p2, *temp;
+
+    p1 = lst->head;
+    p2 = lst->head->nextY;
+
+    if (p1->data == y)
+    { // case y coordinate is in beginnig of the list
+        insertDataToStartYList(lst, y);
+        *found = 1;
+    }
+    else if (p1->data > y)
+    { // case head cooradinate is bigger than y
+        insertDataToStartYList(lst, y);
+    }
+    else // look for y in the middle of the list
+    {
+        while (p2 != NULL)
+        {
+            if (p2->data == y)
+            {
+                insertDataAfterYnode(p2, y);
+                *found = 1;
+                break;
+            }
+            else if (p2->data > y)
+            {
+                insertDataAfterYnode(p1, y);
+                break;
+            }
+            p1 = p1->nextY;
+            p2 = p2->nextY;
+        }
+    }
+    if (p2 == NULL) // case not found in list
+    {
+        insertDataToEndYList(lst, y);
+    }
+}
+
+void insertDataToStartYList(YList *lst, int data)
+{
+    YListNode *newNode;
+    newNode = createNewYNode(data, NULL);
+    insertYNodeToStartList(lst, newNode);
+}
+
+void insertYNodeToStartList(YList *lst, YListNode *newNode)
+{
+    if (isYListEmpty(lst))
+    {
+        lst->head = lst->tail = newNode;
+        newNode->nextY = NULL;
+    }
+    else
+    {
+        newNode->nextY = lst->head;
+        lst->head = newNode;
+    }
+}
+
+void insertDataAfterYnode(YListNode *after, int data)
+{
+    YListNode *newNode;
+    newNode = createNewYNode(data, NULL);
+
+    newNode->nextY = after->nextY;
+    after->nextY = newNode;
+}
+
+XListNode *findXNode(List *lst, int x, int y, int *found)
+{
+    XListNode *res = NULL, *newNode, *p = lst->head;
+
+    while (p != NULL)
+    {
+        if (p->data == x)
+        { // case found node with same x set res to the node and update found to 1
+            res = p;
+            *found = 1;
+            break;
+        }
+        else if (p->data > x)
+        {                                           // case find node with bigger x in list
+            res = createNewXNode(x, y, NULL, NULL); // create new node
+            insertXNodeAfter(p->prevX, res);        // insert node between
+            break;
+        }
+        p = p->nextX;
+    }
+    if (res == NULL)
+    { // case reach to null or empty list
+        res = createNewXNode(x, y, NULL, NULL);
+        insertXNodeToEndList(lst, res);
+    }
+    return res;
+}
+
+void insertXNodeAfter(XListNode *after, XListNode *newNode)
+{
+    newNode->nextX = after->nextX;
+    newNode->prevX = after;
+    after->nextX->prevX = newNode;
+    after->nextX = newNode;
+}
+
+void printList(List *lst)
+{
+    XListNode *currX;
+    YListNode *currY;
+
+    currX = lst->head;
+    currY = lst->head->listOfY.head;
+
+    while (currX != NULL)
+    {
+        while (currY != NULL)
+        {
+            printf("(%d,%d) ", currX->data, currY->data);
+            currY = currY->nextY;
+        }
+        currX = currX->nextX;
+        if (currX != NULL)
+        { // To avoid case currX is null
+            currY = currX->listOfY.head;
+        }
+    }
 }
