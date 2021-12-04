f9720a69bddc6b7fce042c9f3fed93c93e9280e3
Tal Koren <talko4245@gmail.com>
2021-12-04 03:38:29 +0200

    finished task4 -q3

--- task4/q3/q3Funcs.c
+++ task4/q3/q3Funcs.c
@@ -1,10 +1,9 @@
 #include "q3Funcs.h"
 
-TreeNode* createNewTreeNode(int data, TreeNode* parent, TreeNode* left,TreeNode* right)
-{
-    TreeNode* new;
+TreeNode *createNewTreeNode(int data, TreeNode *parent, TreeNode *left, TreeNode *right) {
+    TreeNode *new;
 
-    new = (TreeNode*)malloc(sizeof(TreeNode));
+    new = (TreeNode *) malloc(sizeof(TreeNode));
     checkMemoryAllocation(new);
 
     new->data = data;
@@ -15,26 +14,22 @@ TreeNode* createNewTreeNode(int data, TreeNode* parent, TreeNode* left,TreeNode*
     return new;
 }
 
-void checkMemoryAllocation(void *ptr)
-{
-    if (ptr == NULL)
-    {
-        printf("Error- memory allocation falid\n");
+void checkMemoryAllocation(void *ptr) {
+    if (ptr == NULL) {
+        printf("Error- memory allocation failed\n");
         exit(1);
     }
 }
 
-Tree BuildTreeFromArrayWithLeafList(int *arr, int size)
-{
+Tree BuildTreeFromArrayWithLeafList(int *arr, int size) {
     Tree res;
-    
+
     makeEmptyList(&(res.leafList));
     res.root = BuildTreeFromArrayWithLeafListHelper(arr, size, &(res.leafList));
     return res;
 }
 
-TreeNode *BuildTreeFromArrayWithLeafListHelper(int *arr, int size, List* lst)
-{
+TreeNode *BuildTreeFromArrayWithLeafListHelper(int *arr, int size, List *lst) {
     TreeNode *root;
 
     if (size == 0) // Case get empty array
@@ -50,26 +45,24 @@ TreeNode *BuildTreeFromArrayWithLeafListHelper(int *arr, int size, List* lst)
     root->left = BuildTreeFromArrayWithLeafListHelper(arr, size / 2, lst);
     root->right = BuildTreeFromArrayWithLeafListHelper(arr + ((size / 2) + 1), size / 2, lst);
 
-    if(root->left == NULL && root->right == NULL)//Update leaves list
+    if (root->left == NULL && root->right == NULL)//Update leaves list
     {
         insertDataToEndList(lst, root->data);
     }
-    if(root->left != NULL){
+    if (root->left != NULL) {
         root->left->parent = root;
     }
-    if(root->right != NULL)
-    {
+    if (root->right != NULL) {
         root->right->parent = root;
     }
 
     return root;
 }
 
-ListNode* createNewListNode(int data, ListNode* pNext)
-{
-    ListNode* new;
+ListNode *createNewListNode(int data, ListNode *pNext) {
+    ListNode *new;
 
-    new = (ListNode*)malloc(sizeof(ListNode*));
+    new = (ListNode *) malloc(sizeof(ListNode *));
     checkMemoryAllocation(new);
 
     new->data = data;
@@ -78,68 +71,54 @@ ListNode* createNewListNode(int data, ListNode* pNext)
     return new;
 }
 
-void makeEmptyList(List* lst)
-{
+void makeEmptyList(List *lst) {
     lst->head = lst->tail = NULL;
 }
 
-void insertDataToEndList(List* lst, int data)
-{
-    ListNode* new;
+void insertDataToEndList(List *lst, int data) {
+    ListNode *new;
     new = createNewListNode(data, NULL);
     insertNodeToEndList(lst, new);
 }
 
-void insertNodeToEndList(List* lst, ListNode* new)
-{
-    if(isEmptyList(lst))
-    {
+void insertNodeToEndList(List *lst, ListNode *new) {
+    if (isEmptyList(lst)) {
         lst->head = lst->tail = new;
-    }
-    else
-    {
+    } else {
         lst->tail->next = new;
         lst->tail = new;
     }
     new->next = NULL;
 }
 
-bool isEmptyList(List* lst)
-{
-    if(lst->head == NULL)
-    {
+bool isEmptyList(List *lst) {
+    if (lst->head == NULL) {
         return true;
     }
     return false;
 }
 
-void printTreeInorder(Tree tr)
-{
-    printTreeInOrderHepler(tr.root);
+void printTreeInorder(Tree tr) {
+    printTreeInOrderHelper(tr.root);
     printf("\n");
 }
 
-void printTreeInOrderHepler(TreeNode *root)
-{
-    if (root == NULL)
-    {
+void printTreeInOrderHelper(TreeNode *root) {
+    if (root == NULL) {
         return;
     }
-    printTreeInOrderHepler(root->left);  // Print left sub tree
+    printTreeInOrderHelper(root->left);  // Print left sub tree
     printf("%d ", root->data);           // print root
-    printTreeInOrderHepler(root->right); // print right sub tree
+    printTreeInOrderHelper(root->right); // print right subtree
 }
 
-void freeTree(Tree tr)
-{
+void freeTree(Tree tr) {
     freeTreeHelper(tr.root);
     freeList(&(tr.leafList));
 }
 
-void freeTreeHelper(TreeNode* root)
-{
-    if(root == NULL)
-    {
+void freeTreeHelper(TreeNode *root) {
+    if (root == NULL) {
         return;
     }
     freeTreeHelper(root->left);
@@ -147,28 +126,186 @@ void freeTreeHelper(TreeNode* root)
     free(root);
 }
 
-void freeList(List* lst)
-{
-    ListNode* saver;
+void freeList(List *lst) {
+    ListNode *saver;
 
-    while(lst->head != NULL){
+    while (lst->head != NULL) {
         saver = lst->head;
         lst->head = lst->head->next;
         free(saver);
     }
 }
 
-void printLeafList(Tree tr)
-{
+void printLeafList(Tree tr) {
     PrintList(&(tr.leafList));
 }
 
-void PrintList(List* lst)
-{
-    ListNode* temp = lst->head;
-    while(temp != NULL)
-    {
+void PrintList(List *lst) {
+    ListNode *temp = lst->head;
+    while (temp != NULL) {
         printf("%d ", temp->data);
         temp = temp->next;
     }
+}
+
+TreeNode *findParent(Tree tr, int parentData, int branchSelect) {
+    TreeNode *res;
+    res = findParentHelper(tr.root, parentData, branchSelect);
+    return res;
+}
+
+TreeNode *findParentHelper(TreeNode *root, int parentData, int branchSelect) {
+    TreeNode *found = NULL;
+
+    if (root == NULL) //Case empty tree
+    {
+        return NULL;
+    }
+    if (root->data == parentData) {
+        if (branchSelect) {
+            if (root->right == NULL) {
+                return root;
+            }
+        } else {
+            if (root->left == NULL) {
+                return root;
+            }
+        }
+    }
+    found = findParentHelper(root->left, parentData, branchSelect);//Search for node in left subtree
+    if (found == NULL) {
+        found = findParentHelper(root->right, parentData, branchSelect); //search for node in right subtree
+    }
+
+    return found;
+}
+
+Tree AddLeaf(Tree tr, TreeNode *p, int branchSelect, int data) {
+    TreeNode *newTrNode;
+    int saverData;
+    bool isLeafExists = isLeaf(p);
+    newTrNode = createNewTreeNode(data, p, NULL, NULL);
+
+    if (branchSelect) // Insert tree node to tree
+    {
+        p->right = newTrNode;
+    } else {
+        p->left = newTrNode;
+    }
+    if (isLeafExists == true) {
+        replaceData(&(tr.leafList), p->data, data);
+    } else {
+        if (branchSelect) {
+            saverData = findOrientedLeaf(p->left, branchSelect);
+            insertDataInPlace(&(tr.leafList), data, saverData, AFTER);
+        } else {
+            saverData = findOrientedLeaf(p->right, branchSelect);
+            insertDataInPlace(&(tr.leafList), data, saverData, BEFORE);
+        }
+    }
+    return tr;
+}
+
+void replaceData(List *lst, int dataToReplace, int dataToInsert) {
+    ListNode *p1 = lst->head;
+
+    while (p1->data != dataToReplace) {
+        p1 = p1->next;
+    }
+    p1->data = dataToInsert;
+}
+
+void insertDataInPlace(List *lst, int data, int orientedLeafData, int flag) {
+    ListNode *new;
+    new = createNewListNode(data, NULL);
+
+    if (flag == BEFORE) {
+        insertNodeBefore(lst, new, orientedLeafData);
+    } else {
+        insertNodeAfter(lst, new, orientedLeafData);
+    }
+
+}
+
+void insertNodeAfter(List *lst, ListNode *new, int afterData) {
+    ListNode *p1 = lst->head;
+
+    while (p1 != NULL) {
+        if (p1->data == afterData) {
+            new->next = p1->next;
+            p1->next = new;
+            break;
+        }
+        p1 = p1->next;
+    }
+}
+
+void insertNodeBefore(List *lst, ListNode *new, int before) {
+    ListNode *p1, *p2;
+
+    if (lst->head->data == before)//Case new node needs to enter first
+    {
+        insertNodeToStartList(lst, new);
+    } else {
+        p1 = lst->head;
+        p2 = lst->head->next;
+        while (p2 != NULL) {
+            if (p2->data == before) {
+                new->next = p2;
+                p1->next = new;
+                break;
+            }
+            p2 = p2->next;
+            p1 = p1->next;
+        }
+    }
+}
+
+void insertNodeToStartList(List *lst, ListNode *new) {
+    if (isEmptyList(lst)) {
+        lst->head = lst->tail = new;
+        new->next = NULL;
+    } else {
+        new->next = lst->head;
+        lst->head = new;
+    }
+}
+
+int findOrientedLeaf(TreeNode *root, int branchSelect) {
+    int leftSideLeafData, rightSideLeafData;
+    if (root == NULL) {
+        return -1;
+    }
+    leftSideLeafData = findOrientedLeaf(root->left, branchSelect);
+    rightSideLeafData = findOrientedLeaf(root->right, branchSelect);
+
+    switch (branchSelect) {
+        case 1:
+            if (rightSideLeafData != -1) {
+                return rightSideLeafData;
+            } else if (leftSideLeafData != -1) {
+                return leftSideLeafData;
+            } else {
+                return root->data;
+            }
+            break;
+        case 0:
+            if (leftSideLeafData != -1) {
+                return leftSideLeafData;
+            } else if (rightSideLeafData != -1) {
+                return rightSideLeafData;
+            } else {
+                return root->data;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+bool isLeaf(TreeNode *node) {
+    if (node->right == NULL && node->left == NULL) {
+        return true;
+    }
+    return false;
 }
--- task4/q3/q3Funcs.h
+++ task4/q3/q3Funcs.h
@@ -1,53 +1,91 @@
 #ifndef __Q3FUNCS_H
 #define __Q3FUNCS_H
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define LEFT 0
+#define RIGHT 1
+#define BEFORE -1
+#define AFTER 1
+
 //Data structures
-typedef struct listNode
-{
-	int data;
-	struct listNode* next;
+typedef struct listNode {
+    int data;
+    struct listNode *next;
 } ListNode;
 
-typedef struct list
-{
-	ListNode* head;
-	ListNode* tail;
+typedef struct list {
+    ListNode *head;
+    ListNode *tail;
 } List;
 
-typedef struct  treeNode
-{
-	int data;
-	struct treeNode* parent;
-	struct treeNode* left;
-	struct treeNode* right;
+typedef struct treeNode {
+    int data;
+    struct treeNode *parent;
+    struct treeNode *left;
+    struct treeNode *right;
 } TreeNode;
 
-typedef struct tree
-{
-TreeNode* root;
-List leafList;   /*רשימה מקושרת של כל העלים בעץ*/ 
+typedef struct tree {
+    TreeNode *root;
+    List leafList;
 } Tree;
 
 //Functions
 void checkMemoryAllocation(void *);
-TreeNode* createNewTreeNode(int, TreeNode*, TreeNode*,TreeNode*);
-ListNode* createNewListNode(int, ListNode*);
-void makeEmptyList(List*);
-void insertDataToEndList(List*, int);
-void insertNodeToEndList(List*, ListNode*);
-bool isEmptyList(List* lst);
+
+TreeNode *createNewTreeNode(int, TreeNode *, TreeNode *, TreeNode *);
+
+ListNode *createNewListNode(int, ListNode *);
+
+void makeEmptyList(List *);
+
+void insertDataToEndList(List *, int);
+
+void insertNodeToEndList(List *, ListNode *);
+
+bool isEmptyList(List *lst);
+
 Tree BuildTreeFromArrayWithLeafList(int *, int);
-TreeNode *BuildTreeFromArrayWithLeafListHelper(int *, int, List*);
+
+TreeNode *BuildTreeFromArrayWithLeafListHelper(int *, int, List *); //Used inside BuildTreeFromArrayWithLeafList
+
 void printTreeInorder(Tree);
-void printTreeInOrderHepler(TreeNode *);
-void freeTreeHelper(TreeNode*);
-void freeList(List*);
+
+void printTreeInOrderHelper(TreeNode *);// Used inside printTreeInorder
+
+void freeTreeHelper(TreeNode *);// Used inside freeTree
+
+void freeList(List *);
+
 void freeTree(Tree);
+
 void printLeafList(Tree);
-void PrintList(List*);
+
+void PrintList(List *);
+
+TreeNode *findParentHelper(TreeNode *, int, int);//used inside find parent
+
+TreeNode *findParent(Tree, int, int);
+
+void replaceData(List *, int, int);//function replace treeNode data in other data
+
+void insertNodeToStartList(List *, ListNode *);
+
+void insertNodeBefore(List *, ListNode *, int);// Insert node before node with the data passed to the function
+
+void insertNodeAfter(List *, ListNode *, int);//Insert node after the node with the data passed to the function
+
+void insertDataInPlace(List *, int, int,
+                       int);// Create new list node and calls to insertNodeBefore and insertNodeAfter according to flag passed
+
+Tree AddLeaf(Tree tr, TreeNode *p, int branchSelect, int data);
+
+int findOrientedLeaf(TreeNode *root, int branchSelect);// Finds the most right/left leaf in a tree according to flag
+
+bool isLeaf(TreeNode *node);// checks if a node id a leaf*/
 
 #endif
